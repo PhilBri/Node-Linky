f3726a9ba7338290fc02d4d81290abadf1187380
Phil Bri <PhilBri@users.noreply.github.com>
2018-04-11 17:23:24 +0200

    new

--- linky.js
+++ linky.js
@@ -3,7 +3,16 @@ module.exports = function (RED) {
     function linky (config) {
         var moment  = require ('moment'),
             logUrl  = 'https://espace-client-connexion.enedis.fr/auth/UI/Login',
-            apiUrl  = 'https://espace-client-particuliers.enedis.fr/group/espace-particuliers/suivi-de-consommation';
+            apiUrl  = 'https://espace-client-particuliers.enedis.fr/group/espace-particuliers/suivi-de-consommation',
+            Payload  = {
+                p_p_col_pos:        1,
+                p_p_lifecycle:      2,
+                p_p_col_count:      3,
+                p_p_state:          'normal',
+                p_p_mode:           'view',
+                p_p_cacheability:   'cacheLevelPage',
+                p_p_col_id:         'column-1'
+            };
 
         RED.nodes.createNode (this, config);
         this.name   = config.name;
@@ -11,8 +20,22 @@ module.exports = function (RED) {
         this.pass   = this.credentials.password;
         this.req    = require ('superagent').agent();
         this.cookie = null;
+        this.getDatas = function (retDatas) {
+            linky.status ({fill: 'green', shape: 'dot', text: 'Fetching datas...'});
+            linky.req
+                .post (apiUrl)
+                .type ('form')
+                .send (Payload)
+                .end (function (err, res) {
+                    if (res.error) return linky.error ('Fetching error : ' + res.error + '-' + err.status);
+                    retDatas (JSON.parse (res.text));
+                })
+                . on ('error', function (e) {
+                    return (linky.error ('Getting datas error: ' + e));
+                })
+        }
 
-        this.getCookie = function (cookieStr, retCookie) {
+        var getCookie = function (retCookie) {
             linky.status ({fill: 'grey', shape: 'ring', text: 'Checking Credentials...'});
             linky.req
                 .post (logUrl)
@@ -26,50 +49,27 @@ module.exports = function (RED) {
                 })
                 .end (function (err, res) {
                     if (res.error) return linky.error ('Logging error : ' + res.error + '-' + err.status);
-                    var cookies = (res.request.cookies.split(';'));
+                    var cookies = res.headers['set-cookie'];
                     for (var names in cookies) {
-                        if (cookieStr.test (cookies[names])) 
-                            var mycookie = cookies[names];
-                        if (/^iPlanetDirectoryPro/.test (cookies[names])) 
-                            var mytoken = cookies[names];
+                        if (/^JSESSIONID/.test (cookies[names])) 
+                            retCookie (linky.cookie = cookies[names].split(';')[0]);
                     }
-                    !mytoken || !mycookie ? retCookie (null) : retCookie (mycookie + ';' + mytoken);
+                })
+                . on ('error', function (e) {
+                    return (linky.error ('Getting cookie error: ' + e));
                 })
         }
 
-        this.getDatas = function (datas) {
-            linky.status ({fill: 'green', shape: 'dot', text: 'Fetching datas...'});
-            linky.req
-                .post (apiUrl)
-                .set ('Cookie', linky.cookie)
-                .type ('form')
-                .send (linky.payload)
-                .end (function (err, res) {
-                    if (res.error) return linky.error ('Fetching error : ' + res.error + '-' + err.status);
-                    datas (JSON.parse (res.text));
-                })
-        }
-
-        this.payload = {
-            p_p_col_pos:        1,
-            p_p_lifecycle:      2,
-            p_p_col_count:      3,
-            p_p_state:          'normal',
-            p_p_mode:           'view',
-            p_p_cacheability:   'cacheLevelPage',
-            p_p_col_id:         'column-1'
-        };
         var linky   = this;
 
         if (!linky.user || !linky.pass) {
             linky.status ({fill: 'red', shape: 'dot', text: 'No Credentials !'})
-            return linky.error ('No credentials... Check Username/Password !');
+            return linky.error ('Wrong credentials... Check Username/Password !');
         }
 
-        linky.getCookie (new RegExp ('JSESSIONID'), function (retCookie) {
+        getCookie (function (retCookie) {
             linky.warn (retCookie || 'Can\'t get cookie or token !');
             if (!retCookie) return linky.status ({fill: 'red', shape: 'dot', text: 'Logging error !'});
-            linky.cookie = retCookie;
             linky.status ({fill: 'yellow', shape: 'ring', text: 'Logged-in successfully'});
         });
 
@@ -81,20 +81,20 @@ module.exports = function (RED) {
                 endDate     = moment(msg.payload.fin, "DD-MM-YYYY"),
                 diffDate    = endDate.diff (startDate, 'days');
 
-            linky.payload['_' + ppid + '_dateDebut']    = startDate.format("DD/MM/YYYY");
-            linky.payload['_' + ppid + '_dateFin']      = endDate.format("DD/MM/YYYY");
-            linky.payload['p_p_resource_id']            = 'urlCdcHeure';
-            linky.payload['p_p_id']                     = ppid;
+            Payload['_' + ppid + '_dateDebut']    = startDate.format("DD/MM/YYYY");
+            Payload['_' + ppid + '_dateFin']      = endDate.format("DD/MM/YYYY");
+            Payload['p_p_resource_id']            = 'urlCdcHeure';
+            Payload['p_p_id']                     = ppid;
 
             if (diffDate == 0)
-                linky.payload['_' + ppid + '_dateFin'] = startDate.add (1, 'd').format ("DD/MM/YYYY");
+                Payload['_' + ppid + '_dateFin'] = startDate.add (1, 'd').format ("DD/MM/YYYY");
             else if ((diffDate > 1) && (diffDate < 31))
-                linky.payload['p_p_resource_id'] = 'urlCdcJour';
+                Payload['p_p_resource_id'] = 'urlCdcJour';
             else if (diffDate > 31)
-                linky.payload['p_p_resource_id'] = 'urlCdcMois';
+                Payload['p_p_resource_id'] = 'urlCdcMois';
 
-            linky.getDatas (function (datas) {
-                msg.payload.linky = datas;
+            linky.getDatas (function (retDatas) {
+                msg.payload.linky = retDatas;
                 msg['topic'] = 'linky';
                 linky.send (msg);
                 linky.status ({fill: 'yellow', shape: 'ring', text: 'Logged-in successfully'});
